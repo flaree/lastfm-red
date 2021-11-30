585f25ee28a6520c5cd9b7ac69b0da29e1c7c01b
aleclol <50505980+aleclol@users.noreply.github.com>
2021-11-30 18:53:05 -0500

    1.4.6

--- lastfm/lastfm.py
+++ lastfm/lastfm.py
@@ -48,7 +48,7 @@ class LastFM(
     Interacts with the last.fm API.
     """
 
-    __version__ = "1.4.5"
+    __version__ = "1.4.6"
 
     # noinspection PyMissingConstructor
     def __init__(self, bot, *args, **kwargs):
