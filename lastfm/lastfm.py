fe5ece1ab65bd27a5f3c3b91b048341b9b7a9a91
flaree <flare2399@gmail.com>
2022-07-26 14:42:22 +0100

    v1.6.10

--- lastfm/lastfm.py
+++ lastfm/lastfm.py
@@ -48,7 +48,7 @@ class LastFM(
     Interacts with the last.fm API.
     """
 
-    __version__ = "1.6.9"
+    __version__ = "1.6.10"
 
     # noinspection PyMissingConstructor
     def __init__(self, bot, *args, **kwargs):
